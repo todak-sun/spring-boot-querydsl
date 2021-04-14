728c1841ca6bdb427e7b04a50102396c43a7fa14
todak-sun <tjsdydwn@gmail.com>
2021-04-14 21:53:47 +0900

    init commit

---
+++ src/main/java/io/todak/study/springbootquerydsl/SpringBootQuerydslApplication.java
@@ -0,0 +1,13 @@
+package io.todak.study.springbootquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuerydslApplication.class, args);
+    }
+
+}
